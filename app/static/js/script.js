de74cd2bcc755f1edbd87f2fb2d75c8ab7f269a2
Eric Huang <ehua0017@student.monash.edu>
2025-01-31 23:17:18 +0800

    changes to filtering css

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -3,13 +3,13 @@ function filterUnits() {
     const filter = searchBar.value.toLowerCase();
     const unitCards = document.querySelectorAll('.unit-card');
   
-    unitCards.forEach(card => {
-      const code = card.dataset.code.toLowerCase();
+    unitCards.forEach(card => { // for each card 
+      const code = card.dataset.code.toLowerCase(); //we get its code and name 
       const name = card.dataset.name.toLowerCase();
-      if (code.includes(filter) || name.includes(filter)) {
-        card.style.display = '';
+      if (code.includes(filter) || name.includes(filter)) { //and check if it includes what we have typed
+        card.style.display = ''; // if it is display
       } else {
-        card.style.display = 'none';
+        card.style.display = 'none'; // do not display 
       }
     });
   }
@@ -26,14 +26,14 @@ let votes = {
 function initializeUI() {
   const totalVotes = votes.good + votes.bad;
 
-  document.getElementById('goodVotes').textContent = votes.good;
+  document.getElementById('goodVotes').textContent = votes.good; // setting text
   document.getElementById('badVotes').textContent = votes.bad;
   document.getElementById('totalVotes').textContent = totalVotes;
 
-  const goodPercentage = (votes.good / totalVotes) * 100 || 50;
+  const goodPercentage = (votes.good / totalVotes) * 100 || 50; // to counter the nan case of 0 votes 
   const badPercentage = (votes.bad / totalVotes) * 100 || 50;
 
-  document.getElementById('goodBar').style.width = `${goodPercentage}%`;
+  document.getElementById('goodBar').style.width = `${goodPercentage}%`; // setting size
   document.getElementById('badBar').style.width = `${badPercentage}%`;
 }
 
