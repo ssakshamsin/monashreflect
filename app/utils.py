f4f4d66502dab6a4cf1d13a06386d5fba57af61e
Saksham S <166536380+ssakshamsin@users.noreply.github.com>
2025-02-04 10:40:44 +1100

    hopefully pfp works

--- app/utils.py
+++ app/utils.py
@@ -12,16 +12,23 @@ def save_profile_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(current_app.root_path, 'static\\pictures', picture_fn)
+    picture_path = os.path.join(current_app.root_path, 'static', 'pictures', picture_fn)  # Use os.path.join() here
 
+    # Ensure the directory exists
+    os.makedirs(os.path.dirname(picture_path), exist_ok=True)
+
+    # Remove the old profile picture if it's not the default one
     if current_user.profile_pic != "default.jpg":
-        old_picture_path = os.path.join(current_app.root_path, 'static\\pictures', current_user.profile_pic)
+        old_picture_path = os.path.join(current_app.root_path, 'static', 'pictures', current_user.profile_pic)
         if os.path.exists(old_picture_path):
             os.remove(old_picture_path)
 
-
+    # Resize the image to fit within 150x150 dimensions
     output_size = (150, 150)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
+    
+    # Save the new profile picture
     i.save(picture_path)
+    
     return picture_fn
